81611b39fca389093ea7341737a6ded7e6fb0f70
mambax7 <mambax7@gmail.com>
2020-07-11 22:47:04 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ header.php
@@ -0,0 +1,6 @@
+<?php
+// $Id: header.php,v 1.1 2004/01/29 14:45:56 buennagel Exp $
+
+include "../../mainfile.php";
+
+?>
---
+++ xoops_version.php
@@ -0,0 +1,19 @@
+<?php
+// $Id: xoops_version.php,v 1.1 2004/01/29 14:45:56 buennagel Exp $
+
+$modversion['name'] = ol_videos;
+$modversion['version'] = 1.06;
+$modversion['description'] = ol_DESC;
+$modversion['credits'] = "Online Ludhiana";
+$modversion['author'] = "Rahul Goyal";
+$modversion['license'] = "GPL see LICENSE";
+$modversion['official'] = 0;
+$modversion['image'] = "images/slogo.png";
+$modversion['dirname'] = "ol_videos";
+// Admin
+$modversion['hasAdmin'] = 1;
+$modversion['adminmenu'] = '';
+// Menu
+$modversion['hasMain'] = 1;
+
+?>
